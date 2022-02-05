512d21a6bb751b60a7619695f1ff2433cf249121
Robert Palermo <robert.palermo.official@gmail.com>
2022-02-05 03:49:48 -0500

    Added Bouncy Platforms

---
+++ Assets/[Scripts]/Platforms/BouncyPlatforms.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyPlatforms : MonoBehaviour
+{
+    public float bounceHeight = 10.0f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out ThirdPersonMovement movement))
+        {
+            // Bounce using the ThirdPersonMovement and Character Controller
+            if (ThirdPersonMovement.playerVelocity.y < 0.0f)
+                ThirdPersonMovement.playerVelocity.y += bounceHeight;
+        }
+    }
+}
