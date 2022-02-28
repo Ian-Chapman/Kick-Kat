eb5eab50bfa4649512026bd7bf65b8052b03363b
Robert Palermo <robert.palermo.official@gmail.com>
2022-02-27 21:15:51 -0500

    Fixed Hazards Resetting the Player

--- Assets/[Scripts]/Hazards/HazardScript.cs
+++ Assets/[Scripts]/Hazards/HazardScript.cs
@@ -28,7 +28,29 @@ public class HazardScript : MonoBehaviour
             }
 
             // Reset Player's Position
-            other.transform.position = new Vector3(20.3f, 1.7f, -15.456f);
+            other.gameObject.transform.position = new Vector3(20.3f, 1.7f, -15.456f);
+
+            if (this.tag == "Glass")
+            {
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out ThirdPersonMovement movement))
+        {
+            if (hazardParticleSystems != null && hazardParticleSystems.Count > 0)
+            {
+                foreach (ParticleSystem ps in hazardParticleSystems)
+                {
+                    ps.Play();
+                }
+            }
+
+            // Reset Player's Position
+            other.gameObject.transform.position = new Vector3(20.3f, 1.7f, -15.456f);
 
             if (this.tag == "Glass")
             {
